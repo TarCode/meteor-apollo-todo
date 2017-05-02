fa14bcd33b4a6ef82d5af0c910448da48cccb769
Taariq Isaacs <taariq@uber5.com>
2017-05-02 17:27:04 +0200

    initial commit: meteor-starter-kit boilerplate; Added muicss Appbar

---
+++ imports/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Appbar from 'muicss/lib/react/appbar'
+import Button from 'muicss/lib/react/button'
+import Container from 'muicss/lib/react/container'
+/*
+ * Just a simple presentational component
+ */
+
+const Header = () => (
+  <Appbar/>
+);
+
+export default Header;
